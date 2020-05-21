b59baa6e146b94d95f1aa8cd71499c8b31013e5d
김상헌 <atlanboa@gmail.com>
2020-05-21 22:19:08 +0900

    [add] AuthEntity 추가
    AuthEntity는 트위치, 유투브에 상관없이 OAuth 토큰을 저장하는 테이블입니다.

---
+++ backend/spring-security-login-signup-practice-master/src/main/java/com/rest/api/model/oauth/AuthEntity.java
@@ -0,0 +1,32 @@
+package com.rest.api.model.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "auth")
+public class AuthEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_id")
+    private Long id;
+
+    @Column(length = 100)
+    private String auth_provider;
+
+    @Column(length = 200)
+    private String access_token;
+
+    @Column(length = 200)
+    private String refresh_token;
+
+    @Column(length = 50)
+    private String token_type;
+
+}
