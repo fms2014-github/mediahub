a3c1720888fca73d1f80c0392c178b91568d1fed
김민정 <pacedov3@gmail.com>
2020-05-22 15:58:27 +0900

    메인페이지 병합 - SlideBar & Video
    - Add
    메인페이지에 필요한 컴포넌트(SlideBar, Video) 합쳐서 메인화면 구성
    streaming페이지에 필욯나 컴포넌트(liveChat, liveVideo) 합쳐서 구성
    uploaded, channel 페이지 추가
    
    - Modify
    컴포넌트 병합 후 발생한 CSS버그 수정
    파일 이름 카멜케이스로 통일
    
    - Jira Number
    MainPage 만들기 S02P31D103-63

---
+++ frontend/pages/streaming.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="streaming">
+        <client-only placeholder="loading...">
+            <live-video></live-video>
+        </client-only>
+        <live-chat></live-chat>
+    </div>
+</template>
+
+<script>
+import liveChat from '@/components/liveChat.vue'
+import liveVideo from '@/components/liveVideo.vue'
+
+export default {
+    components: {
+        liveChat,
+        liveVideo,
+    },
+    mounted() {},
+}
+</script>
+
+<style lang="scss" scoped>
+#streaming {
+    display: inline-flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: calc(100% - 74px);
+    height: calc(100vh - 62px);
+    #livechat-component {
+        flex-shrink: 0;
+    }
+}
+</style>
