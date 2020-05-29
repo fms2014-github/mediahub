d84dfb2f656adb7295aabe49f43462e488f9a969
전재욱 <jetacore9219@gmail.com>
2020-05-29 19:34:55 +0900

    배포, 개발 properties 분리 - FIX 배포용 properties 개발용 properties를 분리

--- backend-authentication/src/main/java/com/ssafy/d103/auth/AuthApplication.java
+++ backend-authentication/src/main/java/com/ssafy/d103/auth/AuthApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 public class AuthApplication {
+
     public static void main(String[] args) {
+        // 배포 local 개발 및 테스트 dev
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "local");
         SpringApplication.run(AuthApplication.class, args);
     }
 }
