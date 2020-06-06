d9e5f678f21837136da71e0ffaeefed780b07492
김석용 <fms2014@naver.com>
2020-06-07 04:40:01 +0900

    동기화 로직 수정 구글 API키 다수 추가 AccessToken 적용 방식 수정중...

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,11 +1,11 @@
 export const actions = {
     // nuxtServerInit is called by Nuxt.js before server-rendering every page
     async nuxtServerInit({ commit }, { req }) {
-        if (req.session && req.session.jwt) {
+        if (req.session && req.session.data) {
             try {
                 const data = (await this.$test.get('getTest')).data
             } catch (error) {}
-            commit('login/SET_USER', req.session.jwt)
+            commit('login/SET_USER', req.session.data)
         }
     },
 }
